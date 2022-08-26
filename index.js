79e9489f60f42d24c72c80ca19bb6118e90e75a7
Mustak Absar Khan <MustakHomeFolder@macs-MacBook-Pro.local>
2022-08-26 14:25:03 +0600

    Type Writting Effect: Now Types Line By Line

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ let bodyparaTXT = document.querySelector(".body-p");
 let text = "Build The Community Your Fans Will Love";
 let textp = "Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion."
 
-let speed = 130;
-let speedp = 30;
+let speed = 60;
+let speedp = 70;
 
 let i = 0;
 let p = 0;
 
 const typeWriter = ()=>{
-    if(i<text.length){
+    if(i<text.length){//prints each header text
         bodyheaderTXT.innerHTML += text.charAt(i);
         i++;
         setTimeout(typeWriter,speed);
     };
+    if(bodyheaderTXT.innerHTML.length===39){//checks if the header text is fully printed then it will execute paragraph text running function
+        typeWriterp();
+    };
 };
 
-const typeWriterp = ()=>{
+const typeWriterp = ()=>{//this function runs and prints paragraph text
     if(p<textp.length){
         bodyparaTXT.innerHTML += textp.charAt(p);
         p++;
@@ -26,7 +29,6 @@ const typeWriterp = ()=>{
     };
 };
 
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", ()=>{//this event listener loads the function when the DOM is already loaded
     typeWriter();
-    typeWriterp();
-  });
+});
